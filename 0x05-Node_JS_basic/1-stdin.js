7f925521e0b514f4bdaba31f0ce4962632f44cf3
Danielle Badobre <badobredanielle@gmail.com>
2024-11-26 07:20:10 +0000

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+/**
+ * executed through command line:
+ * display the message Welcome to Holberton School, what is your name?
+ * user input their name on a new line
+ * display Your name is: INPUT
+ * When the user ends the program, display This important software is now closing
+ */
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+    console.log(`Your name is: ${name}`);
+});
+
+rl.on('close', () => {
+    console.log('This important software is now closing\n');
+});
