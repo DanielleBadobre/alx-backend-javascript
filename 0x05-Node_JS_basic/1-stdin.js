fca6d56dc028600e30dd4bb1b80c48094a735f98
Danielle Badobre <badobredanielle@gmail.com>
2024-11-27 08:07:22 +0000

    deleted new line

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -17,5 +17,5 @@ rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
 });
 
 rl.on('close', () => {
-  console.log('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
