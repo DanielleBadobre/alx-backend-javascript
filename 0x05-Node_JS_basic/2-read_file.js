423e3655b7dce5ff978ddd4f2039418572e7cb8a
Danielle Badobre <badobredanielle@gmail.com>
2024-11-27 17:15:00 +0000

    task 2

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+    const lines = data.trim().split('\n');
+
+    const header = lines[0].split(',');
+    const studentData = lines.slice(1).map((line) => line.split(','));
+    console.log(`Number of students: ${studentData.length}`);
+
+    const fields = {};
+    studentData.forEach((student) => {
+      const field = student[3];
+      const name = student[0];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(name);
+    });
+
+    for (const [field, students] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
